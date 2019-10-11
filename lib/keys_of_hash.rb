6c9c0c2afd3f0fa4817c77321b8d441835affce6
Mei Wong <fongmeiw@gmail.com>
2019-10-11 18:47:44 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,20 +1,19 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    def keys_of(*arguments)
     array = []
-    case arguments
-      when "Panama"
-        array[0] = "red-footed tortoise"
-      when "Madagascar"
-        array[0] = "aye-aye"
-        array[1] = "tomato frog"
-      when "Australia"
-        array[0] = "sugar glider"
-        array[1] = "kangaroo"
-        array[2] = "koala"
-      when 1
-        array[0] = :a
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+        array.push(key)
+        end
+      end 
+
+
     end
-    array
+    return array
   end
+  
+end 
 end
