d7e23bd737eb09559a07a2a4fcbaeacf0238a9de
Mei Wong <fongmeiw@gmail.com>
2019-10-11 18:36:10 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    array = []
+    case arguments
+      when "Panama"
+        array[0] = "red-footed tortoise"
+      when "Madagascar"
+        array[0] = "aye-aye"
+        array[1] = "tomato frog"
+      when "Australia"
+        array[0] = "sugar glider"
+        array[1] = "kangaroo"
+        array[2] = "koala"
+      when 1
+        array[0] = :a
+    end
+    array
   end
 end
